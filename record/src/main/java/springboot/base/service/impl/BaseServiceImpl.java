1f0a528bc4ad79f68ed734d05fb4a35159fcbec7
mitoly <mitoly@163.com>
2019-04-01 16:48:05 +0800

    后端 Service方法中添加事务

--- record/src/main/java/springboot/base/service/impl/BaseServiceImpl.java
+++ record/src/main/java/springboot/base/service/impl/BaseServiceImpl.java
@@ -1,5 +1,6 @@
 package springboot.base.service.impl;
 
+import org.springframework.transaction.annotation.Transactional;
 import springboot.base.service.BaseService;
 import springboot.masterdata.user.vo.UserVo;
 import springboot.util.ConstantUtils;
@@ -7,6 +8,7 @@ import springboot.util.ConstantUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Transactional()
 public class BaseServiceImpl implements BaseService {
     public UserVo getCurrentUser(HttpServletRequest request, HttpServletResponse response){
         return (UserVo) request.getSession().getAttribute(ConstantUtils.CURRENT_USER);
