ac36df317458dbb69404185518e2a28bcfc93e72
mitoly <mitoly@163.com>
2020-04-24 11:44:22 +0800

    优化描述 & WebMvcConfigurerAdapter过时改用WebMvcConfigurer

--- record/src/main/java/springboot/webconfig/WebConfig.java
+++ record/src/main/java/springboot/webconfig/WebConfig.java
@@ -3,12 +3,11 @@ package springboot.webconfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springboot.base.interceptor.CheckLoginInterceptor;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter{
+public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private CheckLoginInterceptor checkLoginInterceptor;
